bbb4c4193c8a8e3e98c627ac09b9b94898484ff0
Spyder #DerpGang <spyderzzz.cbc@gmail.com>
2019-08-25 21:45:24 +0530

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -848,7 +848,7 @@ async def hacking (hacked):
         if await hacked.get_reply_message():
             await hacked.edit(
                 "`Targeted Account Hacked successfully 😎......`\n"
-                "`Pay 6969$ to` @shivamkchoudhary `To Remove This Hack...`\n"
+                "`Pay 6969$ to` @spyderzzz `To Remove This Hack...`\n"
             )
 			  
 @register(outgoing=True, pattern="^.kill$")
