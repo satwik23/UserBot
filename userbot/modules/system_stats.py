f0377e4cf8c797a356d77965dba38fd588327464
Spyder #DerpGang <spyderzzz.cbc@gmail.com>
2019-08-26 09:10:09 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -136,7 +136,7 @@ async def amireallyalive(alive):
         await alive.edit(
            "`"
            "Guruji hum abhi zinda hai ;) \n\n"
-            f"User: {DEFAULTUSER} \n"
+            f"Owned by pero @Spyderzzz sar \n"
             "`"
             )
 
