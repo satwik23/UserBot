e838a76d9c7d4fa522db3132f9c6f0eb28be8e60
Spyder #DerpGang <spyderzzz.cbc@gmail.com>
2019-08-26 08:13:32 +0530

    Update create.py

--- userbot/modules/create.py
+++ userbot/modules/create.py
@@ -30,7 +30,7 @@ async def telegraphs(grop):
                 result = await grop.client(functions.messages.ExportChatInviteRequest(
                     peer=created_chat_id,
                 ))
-                await grop.edit("Your `{}` Group Created Successfully. Join [{}]({})".format(group_name, group_name, result.link))
+                await grop.edit("Your `{}` Group bnadia Sar. Join [{}]({})".format(group_name, group_name, result.link))
             except Exception as e:  # pylint:disable=C0103,W0703
                 await grop.edit(str(e))
         elif type_of_group == "g" or type_of_group == "c":
@@ -44,7 +44,7 @@ async def telegraphs(grop):
                 result = await grop.client(functions.messages.ExportChatInviteRequest(
                     peer=created_chat_id,
                 ))
-                await grop.edit("Your `{}` Group/Channel Created Successfully. Join [{}]({})".format(group_name, group_name, result.link))
+                await grop.edit("Your `{}` Group/Channel banadiya sar. Join [{}]({})".format(group_name, group_name, result.link))
             except Exception as e:  # pylint:disable=C0103,W0703
                 await grop.edit(str(e))
 
